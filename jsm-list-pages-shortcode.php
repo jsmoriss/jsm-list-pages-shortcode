da8c271c3cbef0001b7320bcb2d011c4e977e18b
JS Morisset <jsm@surniaulula.com>
2021-11-26 11:11:48 -0800

    v1.0

--- jsm-list-pages-shortcode.php
+++ jsm-list-pages-shortcode.php
@@ -33,48 +33,48 @@ if ( ! defined( 'ABSPATH' ) ) {
 if ( ! class_exists( 'JsmLpsc' ) ) {
 
 	class JsmLpsc {
-	
+
 		public static $children = 0;
-	
+
 		private static $instance = null;	// Wpsso class object.
-	
+
 		public function __construct() {
-	
+
 			add_shortcode( 'child-pages', array( 'JsmLpsc', 'shortcode_list_pages' ) );
 			add_shortcode( 'sibling-pages', array( 'JsmLpsc', 'shortcode_list_pages' ) );
 			add_shortcode( 'list-pages', array( 'JsmLpsc', 'shortcode_list_pages' ) );
-	
+
 			add_filter( 'list_pages_shortcode_excerpt', array( 'JsmLpsc', 'excerpt_filter' ) );
 		}
-	
+
 		public static function &get_instance() {
-	
+
 			if ( null === self::$instance ) {
-	
+
 				self::$instance = new self;
 			}
-	
+
 			return self::$instance;
 		}
-	
+
 		static function shortcode_list_pages( $atts, $content, $tag ) {
-	
+
 			global $post;
-	
+
 			/**
 			 * Child pages.
 			 */
 			$child_of = 0;
-	
+
 			if ( 'child-pages' === $tag ) {
-	
+
 				$child_of = $post->ID;
-	
+
 			} elseif ( 'sibling-pages' === $tag ) {
-	
+
 				$child_of = $post->post_parent;
 			}
-	
+
 			/**
 			 * Set defaults.
 			 */
@@ -103,76 +103,76 @@ if ( ! class_exists( 'JsmLpsc' ) ) {
 				'exclude_current_page' => 0,
 				'excerpt'              => 0
 			);
-	
+
 			/**
 			 * Merge user provided atts with defaults.
 			 */
 			$allowed_atts = shortcode_atts( $defaults, $atts );
-	
+
 			$allowed_atts[ 'title_li' ] = html_entity_decode( $allowed_atts[ 'title_li' ] );
-	
+
 			if ( $allowed_atts[ 'exclude_current_page' ] && absint( $post->ID ) ) {
-	
+
 				if ( ! empty( $allowed_atts[ 'exclude' ] ) ) {
-	
+
 					$allowed_atts[ 'exclude' ] .= ',';
 				}
-	
+
 				$allowed_atts[ 'exclude' ] .= $post->ID;
 			}
-	
+
 			$allowed_atts[ 'echo' ] = 0;
-	
+
 			/**
 			 * Catch <ul> tags in wp_list_pages().
 			 */
 			$allowed_atts[ 'list_type' ] = self::validate_list_type( $allowed_atts[ 'list_type' ] );
-	
+
 			if ( 'ul' !== $allowed_atts[ 'list_type' ] ) {
-	
+
 				add_filter( 'wp_list_pages', array( 'JsmLpsc', 'ul2list_type' ), 10, 2 );
 			}
-	
+
 			/**
 			 * Create output.
 			 */
 			$list_pages_atts = $allowed_atts;
-	
+
 			$list_pages_atts[ 'shortcode_tag' ] = $tag;
-	
+
 			if ( empty( $list_pages_atts[ 'list_type' ] ) ) {
-	
+
 				$list_pages_atts[ 'list_type' ] = 'ul';
 			}
-	
+
 			$output = wp_list_pages( $list_pages_atts );
-	
+
 			if ( 'ul' !== $allowed_atts[ 'list_type' ] ) {
-	
+
 				remove_filter( 'wp_list_pages', array( 'JsmLpsc', 'ul2list_type' ), 10 );
 			}
-	
+
 			if ( ! empty( $output ) && ! empty( $allowed_atts[ 'list_type' ] ) ) {
-	
+
 				$output = '<' . $allowed_atts[ 'list_type' ] . ' class="' . $allowed_atts[ 'class' ] . '">' .
 					$output . '</' . $allowed_atts[ 'list_type' ] . '>';
-	
+
 				/**
 				 * Start from largest number and inverse the child-# class name with a parent-# class name.
 				 */
 				$parent = 0;
-	
+
 				foreach ( array_reverse( range( 0, self::$children ) ) as $num ) {
-	
+
 					$output = preg_replace( '/child-' . $num . '/', '$0 parent-' . $parent, $output );
-	
+
 					$parent++;
 				}
 			}
-	
+
 			return $output;
 		}
-	
+
 		/**
 		 * UL 2 List Type
 		 * Replaces all <ul> tags with <{list_type}> tags.
@@ -182,29 +182,29 @@ if ( ! class_exists( 'JsmLpsc' ) ) {
 		 * @return string HTML output.
 		 */
 		static function ul2list_type( $output, $args = null ) {
-	
+
 			$list_type = self::validate_list_type( $args[ 'list_type' ] );
-	
+
 			if ( 'ul' !== $list_type ) {
-	
+
 				// <ul>
 				$output = str_replace( '<ul>', '<' . $list_type . '>', $output );
 				$output = str_replace( '<ul ', '<' . $list_type . ' ', $output );
 				$output = str_replace( '</ul> ', '</' . $list_type . '>', $output );
-	
+
 				// <li>
 				$list_type = 'span' == $list_type ? 'span' : 'div';
-	
+
 				$output = str_replace( '<li>', '<' . $list_type . '>', $output );
 				$output = str_replace( '<li ', '<' . $list_type . ' ', $output );
 				$output = str_replace( '</li> ', '</' . $list_type . '>', $output );
-	
+
 			}
-	
+
 			return $output;
-	
+
 		}
-	
+
 		/**
 		 * Excerpt Filter
 		 * Add a <div> around the excerpt by default.
@@ -213,15 +213,15 @@ if ( ! class_exists( 'JsmLpsc' ) ) {
 		 * @return string Filtered excerpt.
 		 */
 		static function excerpt_filter( $text ) {
-	
+
 			if ( ! empty( $text ) ) {
-	
+
 				return ' <div class="excerpt">' . $text . '</div>';
 			}
-	
+
 			return $text;
 		}
-	
+
 		/**
 		 * Validate List Type
 		 *
@@ -229,58 +229,58 @@ if ( ! class_exists( 'JsmLpsc' ) ) {
 		 * @return  string              Valid tag.
 		 */
 		public static function validate_list_type( $list_type ) {
-	
+
 			if ( empty( $list_type ) || ! in_array( $list_type, array( 'ul', 'div', 'span', 'article', 'aside', 'section' ) ) ) {
-	
+
 				$list_type = 'ul';
 			}
-	
+
 			return $list_type;
-	
+
 		}
 	}
-	
+
 	/**
 	 * Create HTML list of pages.
 	 */
 	class JsmLpscWalkerPage extends Walker_Page {
-	
+
 		public function display_element( $element, &$children_elements, $max_depth, $depth, $args, &$output ) {
-	
+
 			$id_field = $this->db_fields['id'];
 			$id       = $element->$id_field;
-	
+
 			/**
 			 * Maybe re-sort the children if we have the same shortcode with different sort attributes in the post content.
 			 */
 			if ( ! empty( $args[ 0 ][ 'shortcode_tag' ] ) && ! empty( $children_elements[ $id ] ) ) {
-	
+
 				$tag     = $args[ 0 ][ 'shortcode_tag' ];
 				$pattern = get_shortcode_regex( array( $tag ) );
-	
+
 				if ( preg_match( '/' . get_shortcode_regex() . '/', $element->post_content, $matches ) ) {
-	
+
 					$atts = shortcode_parse_atts( $matches[ 3 ] );
-	
+
 					if ( ! empty( $atts ) ) {
-	
+
 						$def_orderby = $new_orderby = $args[ 0 ][ 'sort_column' ];
 						$def_order   = $new_order   = $args[ 0 ][ 'sort_order' ];
-	
+
 						$new_orderby = empty( $atts[ 'sort_column' ] ) ? $def_orderby : $atts[ 'sort_column' ];
 						$new_order   = empty( $atts[ 'sort_order' ] ) ? $def_orderby : $atts[ 'sort_order' ];
-	
+
 						if ( $def_orderby !== $new_orderby || $def_order !== $new_order ) {
-	
+
 							$children_elements[ $id ] = wp_list_sort( $children_elements[ $id ], $new_orderby, $new_order );
 						}
 					}
 				}
 			}
-	
+
 			parent::display_element( $element, $children_elements, $max_depth, $depth, $args, $output );
 		}
-	
+
 		/**
 		 * @see Walker::start_lvl()
 		 * @since 2.1.0
@@ -289,19 +289,19 @@ if ( ! class_exists( 'JsmLpsc' ) ) {
 		 * @param int $child Depth of page. Used for padding.
 		 */
 		function start_lvl( &$output, $child = 0, $args = array() ) {
-	
+
 			$indent = str_repeat( "\t", $child );
-	
+
 			$list_type = JsmLpsc::validate_list_type( $args[ 'list_type' ] );
-	
+
 			$output .= "\n" . $indent . '<' . $list_type . ' class="children child-' . ( $child + 1 ) . '">' . "\n";
-	
+
 			if ( $child + 1 > JsmLpsc::$children ) {
-	
+
 				JsmLpsc::$children = $child + 1;
 			}
 		}
-	
+
 		/**
 		 * @see Walker::end_lvl()
 		 * @since 2.1.0
@@ -310,14 +310,14 @@ if ( ! class_exists( 'JsmLpsc' ) ) {
 		 * @param int $child Depth of page. Used for padding.
 		 */
 		function end_lvl( &$output, $child = 0, $args = array() ) {
-	
+
 			$indent = str_repeat( "\t", $child );
-	
+
 			$list_type = JsmLpsc::validate_list_type( $args[ 'list_type' ] );
-	
+
 			$output .= $indent . '</' . $list_type . '>' . "\n";
 		}
-	
+
 		/**
 		 * @see Walker::start_el()
 		 * @since 2.1.0
@@ -329,75 +329,75 @@ if ( ! class_exists( 'JsmLpsc' ) ) {
 		 * @param array $args
 		 */
 		function start_el( &$output, $post_obj, $child = 0, $args = array(), $current_page = 0 ) {
-	
+
 			if ( $child ) {
-	
+
 				$indent = str_repeat( "\t", $child );
-	
+
 			} else {
-	
+
 				$indent = '';
 			}
-	
+
 			extract( $args, EXTR_SKIP );
-	
+
 			$css_class = array( 'page-item', 'page-item-' . $post_obj->ID, 'child-' . $child );
-	
+
 			if ( isset( $args[ 'pages_with_children' ][ $post_obj->ID ] ) ) {
-	
+
 				$css_class[] = 'page_item_has_children';
 			}
-	
+
 			if ( ! empty( $current_page ) ) {
-	
+
 				$_current_page = get_page( $current_page );
-	
+
 				if ( in_array( $post_obj->ID, $_current_page->ancestors ) ) {
-	
+
 					$css_class[] = 'current_page_ancestor';
 				}
-	
+
 				if ( $post_obj->ID == $current_page ) {
-	
+
 					$css_class[] = 'current_page_item';
-	
+
 				} elseif ( $_current_page && $post_obj->ID == $_current_page->post_parent ) {
-	
+
 					$css_class[] = 'current_page_parent';
 				}
-	
+
 			} elseif ( $post_obj->ID == get_option( 'page_for_posts' ) ) {
-	
+
 				$css_class[] = 'current_page_parent';
 			}
-	
+
 			$css_class = implode( ' ', apply_filters( 'page_css_class', $css_class, $post_obj, $child, $args, $current_page ) );
-	
+
 			if ( '' === $post_obj->post_title ) {
-	
+
 				$post_obj->post_title = sprintf( __( '#%d (no title)' ), $post_obj->ID );
 			}
-	
+
 			$post_title = apply_filters( 'the_title', $post_obj->post_title, $post_obj->ID );
-	
+
 			$item = '<a href="' . get_permalink( $post_obj->ID ) . '">' . $link_before . $post_title . $link_after . '</a>';
-	
+
 			if ( ! empty( $show_date ) ) {
-	
+
 				if ( 'modified' == $show_date ) {
-	
+
 					$time = $post_obj->post_modified;
-	
+
 				} else {
-	
+
 					$time = $post_obj->post_date;
 				}
-	
+
 				$item .= ' ' . mysql2date( $date_format, $time );
 			}
-	
+
 			if ( $args[ 'excerpt' ] ) {
-	
+
 				$item .= apply_filters( 'list_pages_shortcode_excerpt', $post_obj->post_excerpt, $post_obj, $child, $args, $current_page );
 			}
 
@@ -406,6 +406,6 @@ if ( ! class_exists( 'JsmLpsc' ) ) {
 			$output .= $indent . '<li class="' . $css_class . '">' . $item;
 		}
 	}
-	
+
 	JsmLpsc::get_instance();
 }
