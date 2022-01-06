df75a6076c007819faaf604c47fec1cecc763511
JS Morisset <jsm@surniaulula.com>
2022-01-06 15:22:06 -0800

    v1.0

--- jsm-list-pages-shortcode.php
+++ jsm-list-pages-shortcode.php
@@ -12,7 +12,7 @@
  * Description: [list-pages], [sibling-pages] and [child-pages] shortcodes to list pages in the content.
  * Requires PHP: 7.2
  * Requires At Least: 5.2
- * Tested Up To: 5.8.2
+ * Tested Up To: 5.8.3
  * Version: 1.0
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
@@ -22,7 +22,7 @@
  *      {bugfix}        Backwards-compatible bug fixes or small improvements.
  *      {stage}.{level} Pre-production release: dev < a (alpha) < b (beta) < rc (release candidate).
  *
- * Copyright 2020-2021 Jean-Sebastien Morisset (https://surniaulula.com/)
+ * Copyright 2020-2022 Jean-Sebastien Morisset (https://surniaulula.com/)
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
