65ac8ce5ef81bd825ac2df908abefeb66838ffce
JS Morisset <jsm@surniaulula.com>
2023-01-19 10:09:03 -0800

    v1.0

--- jsm-list-pages-shortcode.php
+++ jsm-list-pages-shortcode.php
@@ -22,7 +22,7 @@
  *      {bugfix}        Backwards-compatible bug fixes or small improvements.
  *      {stage}.{level} Pre-production release: dev < a (alpha) < b (beta) < rc (release candidate).
  *
- * Copyright 2020-2022 Jean-Sebastien Morisset (https://surniaulula.com/)
+ * Copyright 2020-2023 Jean-Sebastien Morisset (https://surniaulula.com/)
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
