fe7b32b8c71d08fbebcb26c1ac8619d2bd4a5be4
JS Morisset <jsm@surniaulula.com>
2023-10-18 07:18:06 -0700

    v1.0

--- jsm-list-pages-shortcode.php
+++ jsm-list-pages-shortcode.php
@@ -12,7 +12,7 @@
  * Description: [list-pages], [sibling-pages] and [child-pages] shortcodes to list pages in the content.
  * Requires PHP: 7.2.34
  * Requires At Least: 5.5
- * Tested Up To: 6.3.2
+ * Tested Up To: 6.4.0
  * Version: 1.0
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
