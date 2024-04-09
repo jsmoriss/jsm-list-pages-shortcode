6dee75db8cf29335badb58a357cdb8cbaa1e2073
jsmoriss <jsm@surniaulula.com>
2024-04-09 22:24:57 +0000

    v1.0

--- jsm-list-pages-shortcode.php
+++ jsm-list-pages-shortcode.php
@@ -12,7 +12,7 @@
  * Description: [list-pages], [sibling-pages] and [child-pages] shortcodes to list pages in the content.
  * Requires PHP: 7.2.34
  * Requires At Least: 5.8
- * Tested Up To: 6.5.0
+ * Tested Up To: 6.5.2
  * Version: 1.0
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
